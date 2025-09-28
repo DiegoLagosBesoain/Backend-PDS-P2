7d759dfbc0ccfb1b3c35cdaa9c852220f2526d8c
DiegoLagosBesoain <dnlagos@miuandes.cl>
2025-09-28 17:50:14 -0300

    arreglando el pool

--- src/db/index.js
+++ src/db/index.js
@@ -2,11 +2,11 @@ const { Pool } = require("pg");
 require("dotenv").config();
 console.log(typeof process.env.PGPASSWORD, process.env.PGPASSWORD);
 const pool = new Pool({
-  host: process.env.PGHOST || "localhost",
-  port: process.env.PGPORT || 5432,
-  database: process.env.PGDATABASE || "PDS",
-  user: process.env.PGUSER ,
-  password: process.env.PGPASSWORD ,
+  host: process.env.DBHOST || "localhost",
+  port: process.env.DBPORT || 5432,
+  database: process.env.DBDATABASE || "PDS",
+  user: process.env.DBUSER ,
+  password: process.env.DBPASSWORD ,
 });
 
 module.exports = pool;
